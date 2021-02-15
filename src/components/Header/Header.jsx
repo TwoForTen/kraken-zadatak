3caa089c50a602c8c520a9782ccea42a3546b488
Noel Rostohar <noel.rostohar@gmail.com>
2021-02-15 19:59:53 +0100

    Header Design Updates & Redux Setup

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,6 @@ import styles from './header.module.scss'
 const Header = () => {
     return (
         <div className={styles.root}>
-            
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/global.scss';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+
+import reducers from './store';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducers, composeEnhancers());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
